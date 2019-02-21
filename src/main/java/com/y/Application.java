30d3984592073628420f2e35deedabc41c3a584b
gcc <350929819@qq.com>
2019-02-21 11:04:31 +0800

    fix

--- src/main/java/com/y/Application.java
+++ src/main/java/com/y/Application.java
@@ -1,9 +1,10 @@
-package com.yabi;
+package com.y;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
